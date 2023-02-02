416a754a1dfea9406152451cf43508f2925bdbf8
SoNomad <nurmuhammad16203@gmail.com>
2023-02-02 16:12:04 +0300

    PC и PS5. Доб редюс для seat,  библ для mui

--- src/components/pages/PC/PC.jsx
+++ src/components/pages/PC/PC.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import styles from './PC.module.scss'
+import { Typography } from '@mui/material';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import styles from './PC.module.scss';
+import { Link } from 'react-router-dom';
+import SportsEsportsTwoToneIcon from '@mui/icons-material/SportsEsportsTwoTone';
+import SkeletonPorts from './SkeletonPorts';
+import { fetchSeats } from '../../../features/slice/seatSlice';
 
 const PC = () => {
-  const dispatch = useDispatch()
-  return <div>mdnvmvnmdnvcmdc,dv,.</div>
-}
+  const dispatch = useDispatch();
+  const { seats, loading } = useSelector((state) => state.seatSlice);
 
-export default PC
+  useEffect(() => {
+    dispatch(fetchSeats());
+  }, [dispatch]);
+
+  if (loading) {
+    return <SkeletonPorts />;
+  }
+
+  const pcSeats = seats.filter((item) => item.platformType.includes('PC'));
+
+  return (
+    <>
+      <Typography variant="h3" mt={3}>
+        Пункт дислокации
+      </Typography>
+      <div className={styles.wrapper}>
+        <div className={styles.platformImg}></div>
+        <div className={styles.seats}>
+          <div className={styles.seatsItems}>
+            {pcSeats.map((seat) => (
+              <Link to={`/seat/${seat._id}`}>
+                <div className={styles.item}>
+                  <SportsEsportsTwoToneIcon fontSize="large" />
+                </div>
+                <Typography mt={1}>{seat.platformType}</Typography>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PC;
--- src/features/store/store.js
+++ src/features/store/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
-import bookingSlice from '../slice/bookingSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import bookingSlice from '../slice/bookingSlice';
+import seatSlice from '../slice/seatSlice';
 
 export const store = configureStore({
   reducer: {
     bookingSlice,
+    seatSlice,
   },
-})
+});
